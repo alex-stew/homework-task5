3cb70c5789a3dad0daf8ca5397bb925e3d10816a
Dawn Treader <69836062+alex-stew@users.noreply.github.com>
2021-01-12 22:01:20 +1030

    added OR function to array on reload to determine if array had saved data

--- Develop/script.js
+++ Develop/script.js
@@ -1,69 +1,70 @@
 // sets an id against each of the days' hour timeblocks to input tasks and save against in localstorage
-var today = [
+var today = JSON.parse(localStorage.getItem("today")) || [
     {
-        id:"0",
-        hour:"09",
-        time:"9",
-        amPm:"AM",
-        task:""
+        id: "0",
+        hour: "09",
+        time: "9",
+        amPm: "AM",
+        task: "",
     },
     {
-        id:"1",
-        hour:"10",
-        time:"10",
-        amPm:"AM",
-        task:""
+        id: "1",
+        hour: "10",
+        time: "10",
+        amPm: "AM",
+        task: "",
     },
     {
-        id:"2",
-        hour:"11",
-        time:"11",
-        amPm:"AM",
-        task:""
+        id: "2",
+        hour: "11",
+        time: "11",
+        amPm: "AM",
+        task: "",
     },
     {
-        id:"3",
-        hour:"12",
-        time:"12",
-        amPm:"PM",
-        task:""
+        id: "3",
+        hour: "12",
+        time: "12",
+        amPm: "PM",
+        task: "",
     },
     {
-        id:"4",
-        hour:"13",
-        time:"1",
-        amPm:"PM",
-        task:""
+        id: "4",
+        hour: "13",
+        time: "1",
+        amPm: "PM",
+        task: "",
     },
     {
-        id:"5",
-        hour:"14",
-        time:"2",
-        amPm:"PM",
-        task:""
+        id: "5",
+        hour: "14",
+        time: "2",
+        amPm: "PM",
+        task: "",
     },
     {
-        id:"6",
-        hour:"15",
-        time:"3",
-        amPm:"PM",
-        task:""
+        id: "6",
+        hour: "15",
+        time: "3",
+        amPm: "PM",
+        task: "",
     },
     {
-        id:"7",
-        hour:"16",
-        time:"4",
-        amPm:"PM",
-        task:""
+        id: "7",
+        hour: "16",
+        time: "4",
+        amPm: "PM",
+        task: "",
     },
     {
-        id:"8",
-        hour:"17",
-        time:"5",
-        amPm:"PM",
-        task:""
+        id: "8",
+        hour: "17",
+        time: "5",
+        amPm: "PM",
+        task: "",
     },
 ]
+console.log(today);
 
 // this will get the data for the current day and year
 function getCurrentDay() {
@@ -75,47 +76,49 @@ function getCurrentDay() {
 getCurrentDay();
 
 // this generates the day planners display
-today.forEach(function(thisHour) {
-    
+today.forEach(function (thisHour) {
+
     // timeblock row
     var hourRow = $("<div>")
-        .attr({
-        "class": "row"
-    });
+        .addClass("row");
 
     // hour cell
     var hourTime = $("<div>")
         .text(`${thisHour.time}${thisHour.amPm}`)
-        .attr({
-            "class": "col-1 hour"
-    });
+        .addClass("col-1 hour");
 
     // input (task) field
-    var taskInput = $("<textarea>");
-  
+    var taskInput = $("<textarea>").val(thisHour.task);
+
     taskInput.attr("id", thisHour.id);
     if (thisHour.hour < moment().format("HH")) {
-        taskInput.attr ({
-            "class": "past col-10 description"
-        })
+        taskInput.addClass("past col-10 description")
     } else if (thisHour.hour === moment().format("HH")) {
-        taskInput.attr({
-            "class": "present col-10 description"
-        })
+        taskInput.addClass("present col-10 description")
     } else if (thisHour.hour > moment().format("HH")) {
-        taskInput.attr({
-            "class": "future col-10 description"
-        })
+        taskInput.addClass("future col-10 description")
     }
 
-    // save button
+// save button
     var saveIcon = $("<i class='fas fa-save fa-lg'></i>");
     var saveTask = $("<button>")
-        .attr({
-            "class": "col-1 saveBtn"
-        });
+        .addClass("col-1 saveBtn");
     saveTask.append(saveIcon);
     hourRow.append(hourTime, taskInput, saveTask);
-
+ 
     $(".container").append(hourRow);
 })
+
+// save button saves data inputted to taskInput field to local storage
+$(".saveBtn").on("click",function(event){
+    event.preventDefault();
+    var id = $(this).prev().attr("id");
+    console.log($(this));
+    today[id].task = $(this).prev().val();
+    saveTask();
+})
+
+// this function will save any tasks to local storage
+function saveTask() {
+    localStorage.setItem("today", JSON.stringify(today));
+}
